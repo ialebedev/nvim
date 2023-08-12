64a9cf39615f6cd7322672ee5cc24625cd70759a
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2023-08-12 11:48:54 +0300

    Some fixes.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,17 +3,17 @@ local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.rust_analyzer.setup {
--- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {
-	    diagnostics = {
-		enable = true,
-		experimental = {
-		    enable = true,
-		},
-	    },
+    -- Server-specific settings. See `:help lspconfig-setup`
+    settings = {
+        ['rust-analyzer'] = {
+	        diagnostics = {
+		        enable = true,
+		        experimental = {
+		            enable = true,
+		        },
+	        },
+        },
     },
-  },
 }
 
 -- Global mappings.
@@ -26,21 +26,21 @@ vim.keymap.set('n', '<Leader>ld', vim.diagnostic.setloclist)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<Leader>la', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<Leader>lf', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<Leader>la', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<Leader>lf', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust" },
 
-  sync_install = false,
-  auto_install = true,
+    sync_install = false,
+    auto_install = true,
 
-  highlight = {
-    enable = true,
-  },
+    highlight = {
+        enable = true,
+    },
 }
